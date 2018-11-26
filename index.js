268b10842abae1d3a6269f68db80dc4690a7930b
Manuela  Sanchez <m.sanchez947@hotmail.com>
2018-11-26 19:26:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-var happyHolidaysTo = "Happy holidays!";
+
+
+{function ("happyHolidays") = ("Happy holidays!")}
+
+function happyHolidaysTo(name) {
+return `Happy holidays, ${name}!`
+}
+
+function happyHolidaysTo(holiday, name) {
+return `Happy ${holiday}, ${name}!` 
+}
+
+function holidayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}!`
+}
